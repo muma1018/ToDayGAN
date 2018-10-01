0c06c544d51f7e06872e848bf241c9bb2bd19514
Markus Müller <markus88mueller@gmx.de>
2018-10-01 15:45:41 +0200

    Add files via upload

---
+++ scripts/test_synth_combogan.sh
@@ -0,0 +1,7 @@
+python3 test.py  \
+    --phase test  \
+    --dataroot ./datasets/synth  \
+    --name synth_combogan  \
+    --n_domains 2\
+    --which_epoch 5  \
+    --show_matrix
---
+++ scripts/train_synth_combogan.sh
@@ -0,0 +1,9 @@
+python3 train.py  \
+    --dataroot ./datasets/synth  \
+    --name synth_combogan  \
+    --n_domains 2  \
+    --niter 15  \
+    --niter_decay 2  \
+    --lambda_identity 0.0  \
+    --lambda_forward 0.0  \
+    --gpu_ids -1
