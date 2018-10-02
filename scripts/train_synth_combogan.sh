02849e737a1b7f0fd23c6ec57adcc483c83be019
Markus Müller <markus88mueller@gmx.de>
2018-10-02 17:18:50 +0200

    Update train_synth_combogan.sh

--- scripts/train_synth_combogan.sh
+++ scripts/train_synth_combogan.sh
@@ -2,8 +2,8 @@ python3 train.py  \
     --dataroot ./datasets/synth  \
     --name synth_combogan  \
     --n_domains 2  \
-    --niter 6  \
-    --niter_decay 2  \
+    --niter 100  \
+    --niter_decay 20  \
     --lambda_identity 0.0  \
     --lambda_forward 0.0  \
-    --gpu_ids 0
+    --gpu_ids -1
