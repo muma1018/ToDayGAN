6cbd19742170c30a7769868384177fbaeec62b8a
Markus Müller <markus88mueller@gmx.de>
2018-10-02 12:20:42 +0200

    minor changes, gpu and niter

--- scripts/train_synth_combogan.sh
+++ scripts/train_synth_combogan.sh
@@ -2,8 +2,8 @@ python3 train.py  \
     --dataroot ./datasets/synth  \
     --name synth_combogan  \
     --n_domains 2  \
-    --niter 1  \
+    --niter 6  \
     --niter_decay 2  \
     --lambda_identity 0.0  \
     --lambda_forward 0.0  \
-    --gpu_ids -1
+    --gpu_ids 0
