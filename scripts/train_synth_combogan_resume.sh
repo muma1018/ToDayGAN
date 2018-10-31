121f188f431f4568663918003c9f6bda8b3912b3
Markus Müller <markus88mueller@gmx.de>
2018-10-31 14:32:50 +0100

    Update train_synth_combogan_resume.sh

--- scripts/train_synth_combogan_resume.sh
+++ scripts/train_synth_combogan_resume.sh
@@ -1,9 +1,9 @@
-python train.py  
---continue_train  
---which_epoch 400  
---name synth_all_combogan  
---dataroot ./datasets/synth_all  
---n_domains 2  
---niter 402  
---niter_decay 402  
---gpu_ids -1
+python train.py  \
+    --continue_train  \
+    --which_epoch 560  \
+    --name synth_all_combogan  \
+    --dataroot ./datasets/synth_all  \
+    --n_domains 2  \
+    --niter 570  \
+    --niter_decay 570  \
+    --gpu_ids -1
